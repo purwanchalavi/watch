c428647c7d8ef5407cc86bece14d732622ab3d65
Avinash <savinashm12@gmial.com>
2023-03-01 22:37:51 +0545

    final message

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        avinash:"#7a1400",
+        ram: "#58bbd2",
+      }
+    },
+  },
+  plugins: [],
+}
